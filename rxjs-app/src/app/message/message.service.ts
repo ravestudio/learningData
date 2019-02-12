273c3ab4c7b287308988c7d096f3ff6aaea9c4de
ravestudio <ravestudio@mail.ru>
2019-02-12 17:57:13 +0600

    changes

--- rxjs-app/src/app/message/message.service.ts
+++ rxjs-app/src/app/message/message.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import {  Subject, Observable } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
+import { map, scan } from 'rxjs/operators';
 import {Message} from '../message/message.model';
+
+const initialMessages: Message[] = [];
 
 interface IMessagesOperation extends Function {
   (messages: Message[]): Message[];
@@ -11,6 +14,8 @@ export class MessageService {
 
   //a steam that publishes new messages only once
   newMessages: Subject<Message> = new Subject<Message>();
+
+  messages: Observable<Message[]>; 
   
   // action streams
   create: Subject<Message> = new Subject<Message>();
@@ -18,13 +23,19 @@ export class MessageService {
   updates: Subject<any> = new Subject<any>();
 
   constructor() {
+
+    this.messages = this.updates.pipe(
+      scan((messages: Message[], operation: IMessagesOperation) => {
+        return operation(messages);
+      }, initialMessages)
+    );
   
-    this.create
-      .map( function(message: Message): IMessagesOperation {
+    this.create.pipe(
+      map( function(message: Message): IMessagesOperation {
         return (messages: Message[]) => {
           return messages.concat(message);
         };
-      })
+      }))
       .subscribe(this.updates);
   
   
